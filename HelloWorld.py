4697008d9d372439fa7f0e31a5bc0c774ff954b9
HarrisonLabrecque <146198413+HarrisonLabrecque@users.noreply.github.com>
2024-07-06 10:23:16 -0400

    Add files via upload

---
+++ HelloWorld.py
@@ -0,0 +1,9 @@
+"""
+Author: Harrison Labrecque
+Date: 7/2/2024
+Description: This is a simple Python program that displays "Hello, World!" to the screen.
+"""
+
+#Displays a simple message to the screen.
+print("Hello, World!")
+
