92241164a20b707dcb02a2e7781b71086a2ca1bd
HarrisonLabrecque <146198413+HarrisonLabrecque@users.noreply.github.com>
2024-07-12 07:38:10 -0400

    Update Balloon.cpp

--- Balloon.cpp
+++ Balloon.cpp
@@ -2,22 +2,22 @@
 using namespace std;
 
 int main() {
-   bool isRed;
-   bool isBalloon;
+   bool isRed; // Declare a boolean variable to store whether the object is red
+   bool isBalloon; // Declare a boolean variable to store whether the object is a balloon
 
-   cin >> isRed;
-   cin >> isBalloon;
+   cin >> isRed; // Prompt user to enter a boolean value for isRed
+   cin >> isBalloon; // Prompt user to enter a boolean value for isBalloon
 
-   if (isBalloon == true && isRed == false){
-      cout << "Balloon" << endl;
+   // Check conditions based on the values of isRed and isBalloon
+   if (isBalloon == true && isRed == false) {
+      cout << "Balloon" << endl; // If isBalloon is true and isRed is false, print "Balloon"
    }
-   else if (isBalloon == true && isRed == true)
-   { cout << "Red balloon" << endl;
+   else if (isBalloon == true && isRed == true) {
+      cout << "Red balloon" << endl; // If isBalloon is true and isRed is true, print "Red balloon"
    }
    else {
-      cout << "Not a balloon" << endl;
+      cout << "Not a balloon" << endl; // If isBalloon is false, print "Not a balloon"
    }
-   
 
-   return 0;
+   return 0; // Return 0 to indicate successful execution
 }
