b6e7ccd63f7da7a9978e29b962761674082af4be
HarrisonLabrecque <146198413+HarrisonLabrecque@users.noreply.github.com>
2024-07-12 07:37:28 -0400

    Update isTeenager.cpp

--- isTeenager.cpp
+++ isTeenager.cpp
@@ -2,24 +2,22 @@
 using namespace std;
 
 int main() {
-   bool isTeenager;
-   int kidAge;
+   bool isTeenager; // Declare a boolean variable to store whether the person is a teenager
+   int kidAge; // Declare an integer variable to store the age
 
-   cin >> kidAge;
+   cin >> kidAge; // Prompt user to enter the age and store it in kidAge
 
-   if (kidAge >= 13 && kidAge < 20){
-      isTeenager = true;
-   }
-   else{
-      isTeenager = false;
-   }
-
-   if (isTeenager) {
-      cout << "Teen" << endl;
+   // Check if the age is between 13 and 19 (inclusive)
+   if (kidAge >= 13 && kidAge <= 19) {
+      isTeenager = true; // Set isTeenager to true if the age is within the teenager range
    }
    else {
-      cout << "Not teen" << endl;
+      isTeenager = false; // Set isTeenager to false if the age is outside the teenager range
    }
 
-   return 0;
-}
+   // Output whether the person is a teen or not based on the boolean value of isTeenager
+   if (isTeenager) {
+      cout << "Teen" << endl; // Print "Teen" if isTeenager is true
+   }
+   else {
+      cout << "Not teen" << endl; // Print "Not teen" if
